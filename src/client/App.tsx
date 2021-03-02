101ef1b30190c7d0f7ba826f5cd87f594722746c
Zach Brucker <zach.brucker@gmail.com>
2021-03-01 22:22:20 -0600

    setup electron, webpack, and dependencies

---
+++ src/client/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// type AppProps = { message: 'hello' };
+// const App = ({ message: string = 'hello' }) => <>{message}</>;
+
+const App = () => <div>Use TypeScript with React</div>;
+
+export default App;
