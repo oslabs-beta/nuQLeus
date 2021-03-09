29a1f81b4238c2e9b154b1999a9f3907b5dd8b93
Zach Brucker <zach.brucker@gmail.com>
2021-03-09 11:23:17 -0600

    implemented CORS policy in express-graphql test server; queries sent to /graphql endpoint are now functioning properly

--- __test__/express-server/app.js
+++ __test__/express-server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema');
+const cors = require('cors');
 
 const mongoose = require('mongoose');
+const schema = require('./schema/schema');
 
 const app = express();
+app.use(cors());
 
 /** Initiate graphQL route **/ 
 
