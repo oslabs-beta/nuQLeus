35fe08b9b7ae5ee27840c26ae695a964848856db
Zach Brucker <zach.brucker@gmail.com>
2021-03-13 18:09:51 -0600

    created nuqleus npm package; functionality is partially complete
    Co-authored-by: Jenny Hai <jenny.hai420@gmail.com>

--- __test__/express-server/app.js
+++ __test__/express-server/app.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const cors = require('cors');
-const schema = require('./schema/schema');
 const { applyMiddleware } = require('graphql-middleware');
 const { ResponsePath, responsePathAsArray, GraphQLType } = require('graphql');
-const queryLevelTracing = require('./controllers/query-tracing');
 const fs = require('fs');
 const path = require('path');
 const mongoose = require('mongoose');
+const queryLevelTracing = require('./controllers/query-tracing');
+const schema = require('./schema/schema');
 
 const app = express();
 
-/** Initiate graphQL route **/ 
+/** Initiate graphQL route * */
 
-//app.use(queryLevelTracing)
 app.use(cors());
 app.use(express.json());
 
+/*
 const extensions = ({
   document,
   variables,
@@ -71,33 +71,130 @@ const logInput = async (resolve, root, args, context, info) => {
 }
 
 const schemaWithMiddleware = applyMiddleware(schema, logInput)
-
-app.use(
-  '/graphql',
-  graphqlHTTP((request) => ({
-      schema: schemaWithMiddleware,
-      context: { startTime: Date.now(), queryTimes: [] }, 
-      graphiql: true,
-      extensions,
-    }))
-);
-
-/** Connect to MongoDB **/ 
-
-mongoose.connect('mongodb+srv://jenny:Codesmith41@cluster0.nbdhe.mongodb.net/sample_mflix?retryWrites=true&w=majority', {
-  // options for the connect method to parse the URI
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Connected to Mongo DB.'))
-  .catch(err => console.log(err));
+*/
 
 
-/** Start server on PORT 4000 **/ 
+function modifyOptions(options, extensions) {
 
-app.listen(4000, () => {
-  console.log('Listening on port 4000'); 
+  const traceResolvers = async (resolve, root, args, context, info) => {
+    const startTime = Date.now();
+    const result = await resolve(root, args, context, info);
+    const endTime = Date.now();
+
+    const pathArray = [];
+    let curPath = info.path;
+    do {
+      pathArray.push(curPath.key);
+      curPath = curPath.prev;
+    } while (curPath);
+
+    const resolverData = {
+      path: pathArray.reverse(),
+      startTime: new Date(startTime).toISOString(),
+      endTime: new Date(endTime).toISOString(),
+      fieldName: info.fieldName,
+      duration: endTime - startTime,
+      operation: info.operation.operation,
+      parentType: info.parentType,
+      returnType: info.returnType,
+    };
+
+    context.nuqleusQueryTimes.push(resolverData);
+    return result;
+  };
+
+  const processExtensions = (extensions) => {
+    // If user has existing extensions
+    if(extensions) {
+
+      // if typeof extensions === function;
+      // const newObj = { pastExtensions: extensions, nuQleusTracing: ... }
+      return ({ document, variables, operationName, result, context }) => ({
+        ...extensions,
+        nuQLeusTracing: {
+          startTime: new Date(context.nuqleusStartTime).toISOString(),
+          endTime: new Date(Date.now()).toISOString(),
+          duration: Date.now() - context.nuqleusStartTime,
+          resolvers: context.nuqleusQueryTimes,
+        },
+      });
+    }
+      else {
+      return ({ document, variables, operationName, result, context, }) => ({
+        nuQLeusTracing: {
+          startTime: new Date(context.nuqleusStartTime).toISOString(),
+          endTime: new Date(Date.now()).toISOString(),
+          duration: Date.now() - context.nuqleusStartTime,
+          resolvers: context.nuqleusQueryTimes,
+        },
+      });
+    }
+  }
+
+  // Options will always be an object:
+  const context = options.context
+    ? { ...options.context, nuqleusStartTime: Date.now(), nuqleusQueryTimes: [] }
+    : { nuqleusStartTime: Date.now(), nuqleusQueryTimes: [] };
+  
+  const processedExtensions = processExtensions(options.extensions);
+  
+  return (request) => ({
+    ...options,
+    schema: applyMiddleware(options.schema, traceResolvers),
+    context,
+    extensions: processedExtensions,
+  });
+}
+
+// Users can still access the variables: { document, variables, operationName, result, context } 
+const extensions = ({ context }) => ({
+  runTime: context.startTime,
+  //nuQLeusTracing: nuQLeusExtensions(),
 });
 
+// Users can still access the variables: request, response, graphQLParams
+const options = (request) => ({
+  schema,
+  context: { startTime: 400, queryTimes: [] },
+  graphiql: true,
+  extensions,
+});
 
+const newOptions = modifyOptions(options);
 
+app.use('/graphql', graphqlHTTP(newOptions));
+
+// Testing npm function
+
+// const testOptions = {
+//   schema: {},
+//   context: { startTime: Date.now(), queryTimes: [] },
+//   graphiql: true,
+//   extensions,
+// };
+
+// const output = modifyOptions(testOptions);
+// console.log(output);
+// console.log(modifyOptions(testOptions));
+
+//
+
+/** Connect to MongoDB * */
+
+mongoose
+  .connect(
+    'mongodb+srv://jenny:Codesmith41@cluster0.nbdhe.mongodb.net/sample_mflix?retryWrites=true&w=majority',
+    {
+      // options for the connect method to parse the URI
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log('Connected to Mongo DB.'))
+  .catch((err) => console.log(err));
+
+/** Start server on PORT 4000 * */
+
+app.listen(4000, () => {
+  console.log('Listening on port 4000');
+});
