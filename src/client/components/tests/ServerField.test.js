9c307a49e84bcc98c413932eb9ac98a1b3cc1dcd
jkim000 <joshuakim514@gmail.com>
2021-03-22 15:26:26 -0700

    add enzyme test for GraphContext

--- src/client/components/tests/ServerField.test.js
+++ src/client/components/tests/ServerField.test.js
@@ -1,5 +1,5 @@
 const serverField = require('../ServerField');
 
-test('Description of the test ex: Should output name and age', () => {
+describe('Description of the test ex: Should output name and age', () => {
   // build tests
 });
