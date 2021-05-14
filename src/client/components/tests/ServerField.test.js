b95ef03bcdbeb812dd521cec382f1794cb9eb9b9
Jenny Hai <jenny.hai420@gmail.com>
2021-05-14 14:16:33 -0700

    update babelrc
    Co-authored-by: Joshua Kim <joshuakim514@gmail.com>

--- src/client/components/tests/ServerField.test.js
+++
@@ -1,6 +0,0 @@
-const serverField = require('../ServerField');
-import Enzyme, { shallow, mount, render } from 'enzyme';
-
-describe('Description of the test ex: Should output name and age', () => {
-  // build tests
-});
